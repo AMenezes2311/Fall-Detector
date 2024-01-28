68ae5fd893b59c9cada1120a2ba97ab48739e4a2
André Menezes <andremenezes231@hotmail.com>
2024-01-27 21:45:03 -0500

    Screenshot
    Updated code with screenshot functionality when fall is detected

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
-import cv2  # pip install opencv-python
-import cvzone  # pip install cvzone
+import cv2
+import cvzone
 from cvzone.PoseModule import PoseDetector
 
-video = cv2.VideoCapture(0)
-detector = PoseDetector() 
-"""
-device = input('What is your device? P or C\n')
-if(device == 'C'):
-    h = 1280
-    w = 720
-else:
-    h = 540
-    w = 960
-"""
+video = cv2.VideoCapture('fall.mp4')
+detector = PoseDetector()
+fall_frame = 0
+
 while True:
-    _, frame = video.read()  #Read the current frame
-    img = cv2.resize(frame, (1280, 720))  #Resize the frame  
-    resultado = detector.findPose(img)  #Identify standing pose
-    pontos,bbox = detector.findPosition(img, draw=False)  #Add points to individual
-    if len(pontos)>=1:  #If points were recognized
-        x, y, w, h = bbox['bbox']  #Save coordinates
-        cabeca = pontos[0][1]  #Head position
-        joelho = pontos[26][1]  #Knee position
-        diferenca = joelho-cabeca  #Find the difference between head and knee
+    _, frame = video.read()  # Read the current frame
+    if frame is None:
+        break  # If no frame is read, end the loop
+    img = cv2.resize(frame, (1280, 720))  # Resize the frame
+    result = detector.findPose(img)  # Identify standing pose
+    points, bbox = detector.findPosition(img, draw=False)  # Add points to individual
 
-        if diferenca <=0:  # If difference is 0 or less than 0, fall deteceted
-            cvzone.putTextRect(img,'Fall Detected',(x,y-80),scale=1,thickness=1,colorR=(0,0,255))
+    if len(points) >= 1:  # If points were recognized
+        x, y, w, h = bbox['bbox']  # Save coordinates
+        head = points[0][1]  # Head position
+        knee = points[26][1]  # Knee position
+        difference = knee - head  # Find the difference between head and knee
 
+        if difference <= 0:  # If difference is 0 or less than 0, fall detected
+            fall_frame += 1
+            cvzone.putTextRect(img, 'Fall Detected', (x, y - 80), scale=2, thickness=2, colorR=(0, 0, 255))
+            
+            # Save the current frame as a screenshot
+            if(fall_frame == 1):
+                cv2.imwrite('fall_frame_screenshot.png', img)
+                print("Screenshot of the video frame saved as 'fall_frame_screenshot.png'")
 
     cv2.imshow('Video', img)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # Exit loop when 'q' is pressed
+        break
+
+video.release()
+cv2.destroyAllWindows()
